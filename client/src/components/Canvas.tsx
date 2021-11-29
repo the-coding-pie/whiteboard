edbc99a638408f6c35836bd9010bb6ee90ec4d16
the-coding-pie <thecodingpie@gmail.com>
2021-11-29 08:23:27 +0530

    refactored

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,4 +1,10 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { ColorContext } from "../contexts/ColorContext";
 
 const Canvas = () => {
@@ -6,47 +12,58 @@ const Canvas = () => {
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const { currentColor } = useContext(ColorContext)!;
+  const [currentCoords, setCurrentCoords] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const canvas = canvasRef.current;
 
     if (canvas) {
       // set width and height
-      canvas.width = window.innerWidth / 1.6;
-      canvas.height = 400;
+      canvas.width = window.innerWidth / 1.2;
+      canvas.height = window.innerHeight / 1.2;
 
       setCtx(canvas.getContext("2d"));
     }
   }, [canvasRef]);
 
-  const startDraw = ({
+  const handleMouseDown = ({
     nativeEvent,
   }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-    const { offsetX, offsetY } = nativeEvent;
-
     setIsDrawing(true);
 
-    ctx!.beginPath();
-    ctx!.moveTo(offsetX, offsetY);
-    ctx!.lineCap = "round";
-    ctx!.lineJoin = "round";
-    // set color
-    ctx!.strokeStyle = currentColor;
+    const { offsetX, offsetY } = nativeEvent;
+
+    // starting coordinates
+    setCurrentCoords({ x: offsetX, y: offsetY });
   };
 
-  const endDraw = () => {
-    setIsDrawing(false);
-  };
-
-  const draw = ({
+  const handleMouseMove = ({
     nativeEvent,
   }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     if (!isDrawing) {
       return;
     }
+
     const { offsetX, offsetY } = nativeEvent;
 
+    setCurrentCoords({ x: offsetX, y: offsetY });
+    drawLine(offsetX, offsetY);
+  };
+
+  const handleMouseUp = () => {
+    setIsDrawing(false);
+  };
+
+  const drawLine = (offsetX: number, offsetY: number) => {
+    ctx!.beginPath();
+    ctx!.moveTo(currentCoords.x, currentCoords.y);
     ctx!.lineTo(offsetX, offsetY);
+
+    ctx!.lineCap = "round";
+    ctx!.lineJoin = "round";
+
+    ctx!.strokeStyle = currentColor;
+
     ctx!.stroke();
   };
 
@@ -55,9 +72,9 @@ const Canvas = () => {
       id="canvas"
       className="canvas bg-white shadow rounded"
       ref={canvasRef}
-      onMouseMove={draw}
-      onMouseDown={startDraw}
-      onMouseUp={endDraw}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
     ></canvas>
   );
 };
